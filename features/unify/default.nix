d9e26de2eeccc87e02e9f9aa4ec1081b10a27edf
pl-misuw <suwara.michal@gmail.com>
2020-02-24 22:19:19 +0100

    End of day commit

--- features/unify/default.nix
+++ features/unify/default.nix
@@ -7,24 +7,25 @@
     after = [ "docker.service" "docker.socket" ];
     requires = [ "docker.service" "docker.socket" ];
     script = ''
-      exec ${pkgs.docker}/bin/docker run \
-          --rm \
+      exec ${pkgs.docker}/bin/docker create \
           --name=unifi-controller \
           --network=host \
           -e PUID=1000 \
           -e PGID=1000 \
-          -e MEM_LIMIT=512M\
+          -e MEM_LIMIT=1024M\
+          --restart unless-stopped \
+          --privileged \
           -v /home/plmisuw/share/unify_config:/config \
           linuxserver/unifi-controller:arm64v8-5.12.35-ls49
     '';
     preStop = "${pkgs.docker}/bin/docker stop unifi-controller";
     reload = "${pkgs.docker}/bin/docker restart unifi-controller";
-    serviceConfig = {
-      ExecStartPre = "-${pkgs.docker}/bin/docker rm -f unifi-controller";
-      ExecStopPost = "-${pkgs.docker}/bin/docker rm -f unifi-controller";
-      TimeoutStartSec = 0;
-      TimeoutStopSec = 120;
-      Restart = "unless-stopped";
-    };
+    #serviceConfig = {
+     # ExecStartPre = "${pkgs.docker}/bin/docker rm -f unifi-controller";
+     # ExecStopPost = "${pkgs.docker}/bin/docker rm -f unifi-controller";
+     # TimeoutStartSec = 0;
+     # TimeoutStopSec = 120;
+     # Restart = "always";
+    #};
   };
 }
